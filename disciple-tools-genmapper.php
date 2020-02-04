e96d914ad0cab03eb0b7ca35393a158d1a9d91ce
Chris Chasm <chris@chasm.solutions>
2020-02-04 12:13:25 -0700

    Multisite: Added filter to admin notices for mixed multisite environments have both Disciple Tools and other themes.

--- disciple-tools-genmapper.php
+++ disciple-tools-genmapper.php
@@ -26,7 +26,7 @@ $dt_genmapper_required_dt_theme_version = '0.21.0';
  *
  * @since  0.1
  * @access public
- * @return object
+ * @return object|bool
  */
 function dt_genmapper_metrics() {
     global $dt_genmapper_required_dt_theme_version;
@@ -37,9 +37,12 @@ function dt_genmapper_metrics() {
      */
     $is_theme_dt = strpos( $wp_theme->get_template(), "disciple-tools-theme" ) !== false || $wp_theme->name === "Disciple Tools";
     if ( !$is_theme_dt || version_compare( $version, $dt_genmapper_required_dt_theme_version, "<" ) ) {
-        add_action( 'admin_notices', 'dt_genmapper_metrics_hook_admin_notice' );
-        add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
-        return new WP_Error( 'current_theme_not_dt', 'Disciple Tools Theme not active or not latest version.' );
+        if ( ! is_multisite() ) {
+            add_action( 'admin_notices', 'dt_genmapper_metrics_hook_admin_notice' );
+            add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
+        }
+
+        return false;
     }
     /**
      * Load useful function from the theme
