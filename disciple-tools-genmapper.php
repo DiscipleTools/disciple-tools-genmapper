e950d77c1113cacd318ce3052ffcc86fd4da98cc
Chris Chasm <chris@chasm.solutions>
2021-01-13 12:51:50 -0700

    Added version

--- disciple-tools-genmapper.php
+++ disciple-tools-genmapper.php
@@ -8,7 +8,7 @@
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-advanced-metrics-template
  * Requires at least: 4.7.0
  * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
- * Tested up to: 5.2
+ * Tested up to: 5.6
  *
  * @package Disciple_Tools
  * @link    https://github.com/DiscipleTools
