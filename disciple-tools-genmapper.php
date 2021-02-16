ff3f62d77cd7eb202bdec076251a91e82eab8626
corsacca <corsacca@gmail.com>
2021-02-16 14:36:34 +0100

    Make Plugin Update Checker available on multisite when the active theme is not Disciple.Tools

--- disciple-tools-genmapper.php
+++ disciple-tools-genmapper.php
@@ -79,25 +79,6 @@ class DT_Genmapper_Metrics {
 
         require_once( 'includes/charts/charts-loader.php' );
 
-        if ( is_admin() ) {
-            // Check for plugin updates
-            if ( !class_exists( 'Puc_v4_Factory' ) ) {
-                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-            }
-            /**
-             * Below is the publicly hosted .json file that carries the version information. This file can be hosted
-             * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
-             * a template.
-             * Also, see the instructions for version updating to understand the steps involved.
-             * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
-             */
-            Puc_v4_Factory::buildUpdateChecker(
-                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-genmapper/master/version-control.json',
-                __FILE__,
-                'disciple-tools-genmapper'
-            );
-        }
-
         // Add links to plugin
         if ( is_admin() ) {
             add_filter( 'plugin_action_links_' .  plugin_basename( __FILE__ ), array( &$this, 'plugin_action_links' ) );
@@ -255,3 +236,31 @@ if ( !function_exists( "dt_hook_ajax_notice_handler" )){
         }
     }
 }
+
+
+/**
+ * Check for plugin updates even when the active theme is not Disciple.Tools
+ *
+ * Below is the publicly hosted .json file that carries the version information. This file can be hosted
+ * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
+ * a template.
+ * Also, see the instructions for version updating to understand the steps involved.
+ * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
+ */
+add_action( 'plugins_loaded', function (){
+    if ( is_admin() ){
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-genmapper/master/version-control.json',
+                __FILE__,
+                'disciple-tools-genmapper'
+            );
+        }
+    }
+} );
