63bc5a02e7477b9462a6159be76b2cc7bc6f8d9d
corsacca <corsacca@gmail.com>
2021-02-17 15:41:55 +0100

    Plugin Update Checker fixes and auto update enabled.

--- disciple-tools-genmapper.php
+++ disciple-tools-genmapper.php
@@ -248,11 +248,13 @@ if ( !function_exists( "dt_hook_ajax_notice_handler" )){
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
 add_action( 'plugins_loaded', function (){
-    if ( is_admin() ){
-        // Check for plugin updates
+    if ( is_admin() || wp_doing_cron() ){
         if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
-                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            // find the Disciple.Tools theme and load the plugin update checker.
+            foreach ( wp_get_themes() as $theme ){
+                if ( $theme->get( 'TextDomain' ) === "disciple_tools" && file_exists( $theme->get_stylesheet_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ){
+                    require( $theme->get_stylesheet_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+                }
             }
         }
         if ( class_exists( 'Puc_v4_Factory' ) ){
